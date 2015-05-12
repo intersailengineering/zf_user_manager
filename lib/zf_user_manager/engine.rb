706adbc2e8d9f17752b1babea8c1e2178dd5ee56
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2015-05-12 17:19:20 +0200

    added decorator in engine

--- lib/zf_user_manager/engine.rb
+++ lib/zf_user_manager/engine.rb
@@ -1,5 +1,12 @@
 module ZfUserManager
   class Engine < ::Rails::Engine
     isolate_namespace ZfUserManager
+
+    # Load host application decorators to override classes
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |file|
+        require_dependency(file)
+      end
+    end
   end
 end
