b0dc0ecb335aeab1c45f98de59f09c3cbe20d43c
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2015-09-14 18:07:57 +0200

    added comment

--- app/controllers/intersail/zf_user_manager/application_controller.rb
+++ app/controllers/intersail/zf_user_manager/application_controller.rb
@@ -1,6 +1,7 @@
 module Intersail
   module ZfUserManager
     class ApplicationController < ActionController::Base
+      # this is needed in order to work with rails 4.1
       layout proc{|c| c.request.xhr? ? false : "application_zum" }
 
       before_action :authenticate_user!
